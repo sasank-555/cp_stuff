1cb741ac6ed587e6f4d8081049161b3c9855fa94
Sasank <sasanknasika555@gmail.com>
2024-06-16 13:06:03 +0530

    .

---
+++ CSES/Combinatorics/Distributing_Apples.py
@@ -0,0 +1,48 @@
+########################################################################################################################
+# -----------------------------------------------AUTHOR: shank_555-----------------------------------------------------#
+########################################################################################################################
+import time
+import bisect
+import functools
+import math
+import os
+import random
+import re
+import sys
+import threading
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from io import BytesIO, IOBase
+from itertools import accumulate, combinations, permutations
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase
+from typing import *
+import collections
+import heapq
+import itertools
+alphabets = list("abcdefghijklmnopqrstuvwxyz")
+vowels = list("aeiou")
+MOD1 = int(1e9 + 7)
+MOD2 = 998244353
+INF = int(1e17)
+I = lambda: input()
+II = lambda: int(input())
+MII = lambda: map(int, input().split())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+LGMII = lambda: map(lambda x: int(x) - 1, input().split())
+LGLII = lambda: list(map(lambda x: int(x) - 1, input().split()))
+inf = float('inf')
+fact =[1]
+inv = [1]
+for i in range(1,10**6+1):
+    fact.append(pow(fact[-1]*i,1,MOD1))
+    inv.append(pow(fact[-1],-1,MOD1))
+for _ in range(II()):
+    a,b=LII()
+    a = a+b-1
+    b = b-1
+    print((fact[a]*inv[b]*inv[a-b])%MOD1)
+
