f3be98fb9613133f2f0dd80e4d086d13f2326f97
Sasank <sasanknasika555@gmail.com>
2024-12-04 23:48:48 +0530

    ...

--- Prefix2D.py
+++ Prefix2D.py
@@ -30,3 +30,43 @@ class PreFixSumMatrix:
             self.t(self.pre[xb + 1][yb + 1], -self.pre[xb + 1][ya]),
             -self.t(self.pre[xa][yb + 1], self.pre[xa][ya])
         )
+def apply_2d_difference(n, m, updates):
+    """
+    Apply updates to an n x m grid using a 2D difference array.
+
+    Args:
+        n: Number of rows in the grid.
+        m: Number of columns in the grid.
+        updates: List of updates, where each update is of the form 
+                 (r1, c1, r2, c2, val) representing adding 'val' 
+                 to the subrectangle from (r1, c1) to (r2, c2).
+
+    Returns:
+        The final grid after applying all updates.
+    """
+    # Initialize the difference array
+    diff = [[0] * (m + 1) for _ in range(n + 1)]
+    
+    # Apply all updates
+    for r1, c1, r2, c2, val in updates:
+        diff[r1][c1] += val
+        if r2 + 1 < n:
+            diff[r2 + 1][c1] -= val
+        if c2 + 1 < m:
+            diff[r1][c2 + 1] -= val
+        if r2 + 1 < n and c2 + 1 < m:
+            diff[r2 + 1][c2 + 1] += val
+    
+    # Compute the final grid using prefix sums
+    result = [[0] * m for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            result[i][j] = diff[i][j]
+            if i > 0:
+                result[i][j] += result[i - 1][j]
+            if j > 0:
+                result[i][j] += result[i][j - 1]
+            if i > 0 and j > 0:
+                result[i][j] -= result[i - 1][j - 1]
+    
+    return result
