6b59a9e457fd6be587e96cdddb2d131228db0742
Sasank <sasanknasika555@gmail.com>
2025-01-30 21:18:44 +0530

    ...

---
+++ CSES/Queries/Distinct_Values_Queries.py
@@ -0,0 +1,107 @@
+########################################################################################################################
+# -----------------------------------------------AUTHOR: shank_555-----------------------------------------------------#
+########################################################################################################################
+import time
+from bisect import bisect_left,bisect_right
+import functools
+import math
+import os
+import random
+import re
+import sys
+import threading
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from io import BytesIO, IOBase
+from itertools import accumulate, combinations, permutations
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase
+from typing import *
+import collections
+import heapq
+import itertools
+class SafeCounter(Counter):
+    """
+    A Counter subclass that obfuscates keys using a random 64-bit integer to prevent hash collision attacks.
+    
+    Only supports integer keys by default. Other key types can be supported by converting them to integers.
+    """
+    def __init__(self, *args, r64=None, **kwargs):
+        self.r64 = r64 or random.getrandbits(64)  # Generate a 64-bit random integer if not provided
+        super().__init__(*args, **kwargs)
+
+    def _transform_key(self, key):
+        """Apply an XOR-based transformation to the key."""
+        if not isinstance(key, int):
+            raise TypeError(f"SafeCounter only supports integer keys, got {type(key)}.")
+        return key ^ self.r64
+
+    def __setitem__(self, key, value):
+        transformed_key = self._transform_key(key)
+        super().__setitem__(transformed_key, value)
+
+    def __delitem__(self, key):
+        transformed_key = self._transform_key(key)
+        super().__delitem__(transformed_key)
+
+    def __getitem__(self, key):
+        transformed_key = self._transform_key(key)
+        return super().__getitem__(transformed_key)
+
+    def __contains__(self, key):
+        transformed_key = self._transform_key(key)
+        return super().__contains__(transformed_key)
+def input(): return sys.stdin.readline().rstrip("\r\n")
+alphabets = list("abcdefghijklmnopqrstuvwxyz")
+vowels = list("aeiou")
+MOD1 = int(1e9 + 7)
+MOD2 = 998244353
+INF = int(1e17)
+I = lambda: input()
+II = lambda: int(input())
+MII = lambda: map(int, input().split())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+LGMII = lambda: map(lambda x: int(x) - 1, input().split())
+LGLII = lambda: list(map(lambda x: int(x) - 1, input().split()))
+inf = float('inf')
+def solve():
+    n ,q = LII()
+    nums = LII()    
+    tree  =[0]*(n + 1)
+    def update(i,dx):
+        i+=1
+        while i < len(tree):
+            tree[i]+=dx
+            i+= (i & -i)
+    def query(i):
+        i+=1
+        ret  = 0
+        while i > 0:
+            ret+=tree[i]
+            i-=(-i & i)
+        return ret
+    d = defaultdict(list)
+    for i in range(q):
+        l,r = LGLII()
+        d[l].append([r,i])
+    last_val = {}
+    ans = [-1]*(q)
+    for i in range(n-1,-1,-1):
+        val = nums[i]
+        if val in last_val:
+            update(last_val[val],-1)
+        last_val[val] = i
+        update(i,1)
+        for r,j in d[i]:
+            ans[j] = query(r)
+    for x in ans:
+        print(x)
+
+for _ in range(1):
+    t  = solve()
+    #print(t)
+    #print("YES" if t else "NO")
+    #print("NO" if t else "NO")
